b09d6e8be53a0544499e6cd7168d0083fc21556c
Maciej Trzciński <maciejftrzcinski@gmail.com>
2020-04-20 15:50:39 +0200

    Add files via upload

---
+++ zad2/mymath.h
@@ -0,0 +1,4 @@
+int factorial(int x);
+double first_derivative(double(*fun)(double),double x,double dx);
+double second_derivative(double(*fun)(double),double x,double dx);
+double simple_integral(double(*fun)(double),double x_start,double x_end,double dx);
